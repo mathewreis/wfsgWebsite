27aede3a695673cf7b390648d67a10c88a9a7587
mathewreis <134575402+mathewreis@users.noreply.github.com>
2023-05-26 22:20:40 -0400

    updated files to new version

---
+++ login.js
@@ -0,0 +1,20 @@
+// Get form element
+const loginForm = document.getElementById('login-form');
+
+// Add submit event listener to the form
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Get username and password input values
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // Perform login authentication (example code)
+  if (username === 'mathewreis' && password === 'Mat102509!') {
+    alert('Login successful!');
+    // Redirect to another page after successful login
+    window.location.href = 'https://github.com/floydsg/floydsg.github.io';
+  } else {
+    alert('Login failed. Please try again.');
+  }
+});
